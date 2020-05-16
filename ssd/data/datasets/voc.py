15a386d2041952cd7cfe455ee7d6fa92d3a2eb55
Tao <2761564455@qq.com>
2020-05-16 17:05:20 +0800

    1

--- ssd/data/datasets/voc.py
+++ ssd/data/datasets/voc.py
@@ -47,13 +47,16 @@ class VOCDataset(torch.utils.data.Dataset):
         print("class:",labels)
         if self.transform:
             image, boxes, labels = self.transform(image, boxes, labels) #对原始图片进行转换
-        if self.target_transform:
-            boxes, labels = self.target_transform(boxes, labels) #target_transform指处理GTbox
         print("image.shape after transform:",image.shape)
         print("boxes.shape,class.shape,after transform",boxes.shape,labels.shape)
         print("boxes after transform:",boxes)
         print("class after transform:",labels)
-        
+        if self.target_transform:
+            boxes, labels = self.target_transform(boxes, labels) #target_transform指处理GTbox
+        print("boxes.shape,class.shape,after target_transform",boxes.shape,labels.shape)
+        print("boxes after target_transform:",boxes)
+        print("class after target_transform:",labels)
+
         targets = Container(
             boxes=boxes,
             labels=labels,
